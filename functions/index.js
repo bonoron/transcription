7dea5f2908d2e5957c63bb1d2b79c2a15c321b97
bonoron <minomino04180418@gmail.com>
2023-01-07 22:26:28 +0900

    first commit

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => {
+    res.render('top.ejs');
+});
+
+//exports.app = functions.https.onRequest(app);
+exports.app = functions.region('asia-northeast1').https.onRequest(app);
