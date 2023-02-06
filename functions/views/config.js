4a6ad11d7abb59355ef934935830d63212313cba
bonoron <minomino04180418@gmail.com>
2023-02-06 23:42:43 +0900

    change the way to login

---
+++ functions/views/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJGyDAkuzU2C9Psr0X_iljcJgJpvi2O24",
+    authDomain: "transcription-2e6e2.firebaseapp.com",
+    projectId: "transcription-2e6e2",
+    storageBucket: "transcription-2e6e2.appspot.com",
+    messagingSenderId: "118222486071",
+    appId: "1:118222486071:web:c9571aec0e456c0e949db2",
+    measurementId: "G-XWMY98DN1D"
+    };
+  // firebase の初期化
+firebase.initializeApp(firebaseConfig);
